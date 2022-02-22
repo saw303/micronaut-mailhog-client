68ed0c0af94f1ab376af6d1b8d871066796555f0
Silvio Wangler <71073+saw303@users.noreply.github.com>
2022-02-22 13:51:32 +0100

    Apply license

--- src/main/java/io/wangler/micronaut/mailhog/Contact.java
+++ src/main/java/io/wangler/micronaut/mailhog/Contact.java
@@ -1,24 +1,41 @@
+/*
+ * Copyright 2022 Silvio Wangler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.wangler.micronaut.mailhog;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
 
 @Data
 public class Contact {
 
-    @JsonProperty("Relays")
-    private List<String> relays = new ArrayList<>();
-    @JsonProperty("Mailbox")
-    private String mailbox;
-    @JsonProperty("Domain")
-    private String domain;
-    @JsonProperty("Params")
-    private String params;
+  @JsonProperty("Relays")
+  private List<String> relays = new ArrayList<>();
 
-    public String toEmail() {
-        return getMailbox() + "@" + getDomain();
-    }
+  @JsonProperty("Mailbox")
+  private String mailbox;
+
+  @JsonProperty("Domain")
+  private String domain;
+
+  @JsonProperty("Params")
+  private String params;
+
+  public String toEmail() {
+    return getMailbox() + "@" + getDomain();
+  }
 }
--- src/main/java/io/wangler/micronaut/mailhog/MailHogItem.java
+++ src/main/java/io/wangler/micronaut/mailhog/MailHogItem.java
@@ -1,44 +1,66 @@
+/*
+ * Copyright 2022 Silvio Wangler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.wangler.micronaut.mailhog;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
 
 @Data
 public class MailHogItem {
 
-    @JsonProperty("ID")
-    private String id;
-    @JsonProperty("From")
-    private Contact from;
-    @JsonProperty("To")
-    private List<Contact> to = new ArrayList<>();
-    @JsonProperty("Cc")
-    private List<Contact> cc = new ArrayList<>();
-    @JsonProperty("Bcc")
-    private List<Contact> bcc = new ArrayList<>();
-    @JsonProperty("Content")
-    private Content content;
-    @JsonProperty("Created")
-    private Instant created;
-    @JsonProperty("MIME")
-    private Mime mime;
-    @JsonProperty("Raw")
-    private Raw raw;
+  @JsonProperty("ID")
+  private String id;
 
-    public String subject() {
-        return getContent().readSingleHeaderValue("Subject");
+  @JsonProperty("From")
+  private Contact from;
+
+  @JsonProperty("To")
+  private List<Contact> to = new ArrayList<>();
+
+  @JsonProperty("Cc")
+  private List<Contact> cc = new ArrayList<>();
+
+  @JsonProperty("Bcc")
+  private List<Contact> bcc = new ArrayList<>();
+
+  @JsonProperty("Content")
+  private Content content;
+
+  @JsonProperty("Created")
+  private Instant created;
+
+  @JsonProperty("MIME")
+  private Mime mime;
+
+  @JsonProperty("Raw")
+  private Raw raw;
+
+  public String subject() {
+    return getContent().readSingleHeaderValue("Subject");
+  }
+
+  public String body() {
+    List<Part> parts = getMime().getParts();
+
+    if (parts.isEmpty()) {
+      return null;
     }
-
-    public String body() {
-        List<Part> parts = getMime().getParts();
-
-        if (parts.isEmpty()) {
-            return null;
-        }
-        return parts.get(0).getBody();
-    }
+    return parts.get(0).getBody();
+  }
 }
